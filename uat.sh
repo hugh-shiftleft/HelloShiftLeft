3a8cd1abc051efb31364699c0518e3b82eaa15f0
hugh-shiftleft <49698586+hugh-shiftleft@users.noreply.github.com>
2019-05-31 10:45:32 -0400

    Update uat.sh

--- uat.sh
+++ uat.sh
@@ -1,4 +1,4 @@
-SHIFTLEFT_SEC_COLLECT_ATTACK_INFO=true sl run -- java -jar target/hello-shiftleft-0.0.1.jar &
+sl run -- java -D-Dshiftleft.sec.collect.attack.info=true -jar target/hello-shiftleft-0.0.1.jar &
 sleep 60
 curl http://localhost:8081/customers/1
 curl http://localhost:8081/customers/
